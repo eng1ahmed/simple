ca6e1a0a9db3e924147dce7dd7339213fd5c4d4f
eng1ahmed <engahmed11132@gmail.com>
2024-12-14 19:04:34 +0200

    dpkjk

--- js/signup.js
+++ js/signup.js
@@ -3,16 +3,51 @@ let userName = document.getElementById("userName");
 let password = document.getElementById("Password");
 let email = document.getElementById("email");
 let phone = document.getElementById("phone");
+// ###################
+function validatePassword(password) {
+  const regex =
+    /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[a-zA-Z\d!@#$%^&*()_+]{8,}$/;
+  return regex.test(password);
+}
+function validateUsername(username) {
+  const regex = /^[a-zA-Z]+(?:_[a-zA-Z]+)*$/;
+  const cleanedUsername = username.replace(/\s+/g, "_"); // Replace spaces with underscores
+  return regex.test(cleanedUsername);
+}
+function validateEmail(email) {
+  const regex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(email);
+}
+function validatePhoneNumber(phoneNumber) {
+  const regex = /^\+?(\d{10,13})$/;
+  return regex.test(phoneNumber);
+}
 
+//##########################
 form.onsubmit = addEventListener("submit", (event) => {
-  event.preventDefault();
-  window.location.href = "../index.html";
-  let userData = {
-    username: userName.value,
-    Password: password.value,
-    email: email.value,
-    phone: phone.value,
-  };
-  localStorage.userName = userData.username;
-  localStorage.password = userData.Password;
+  let validateUser = validateUsername(userName.value.toLowerCase());
+  let isValid = validatePassword(password.value);
+  let emailvalid = validateEmail(email.value);
+  let phonevalid = validatePhoneNumber(phone.value);
+  if (validateUser === false) {
+      confirm("username must contain no special charcters and numbers");
+  }
+  if (emailvalid === false) {
+      confirm("please enter valid email");
+  }
+  if (phonevalid === false) {
+      confirm("please enter valid phone number");
+    }
+    if (isValid === false) {
+      confirm(
+        "password must contain at least 1 (special charcter & number & uppercase letter & lower case letter) and must be at least 8 charcter"
+      );
+    }
+    if (validateUser && isValid && emailvalid && phonevalid === true) {
+        window.location.href = "../index.html";
+      localStorage.userName = userName.value;
+      localStorage.password = password.value;
+    }
+    event.preventDefault();
 });
